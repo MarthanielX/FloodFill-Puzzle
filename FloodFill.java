84b52793a68e5ea77e08d0a8b8f2c77e2a618bdc
Nathaniel <sauerergn@carleton.edu>
2017-03-21 14:25:24 -0700

    Working playable game, made Square class for board sqaures instead of ints

--- FloodFill.java
+++ FloodFill.java
@@ -3,20 +3,11 @@ import java.util.*;
 
 public class FloodFill{
 
-   static final String FILE = "input1.txt";
-   static Integer[][] board;
+   static final String FILE = "Input2.txt";
+   static Square[][] board;
    static int size;
-   
-   public static class Duple {
-      int row;
-      int col;
-      public Duple(int a, int b){
-         row = a;
-         col = b;
-      }
-   } 
 
-   public static void main (String args[]) throws FileNotFoundException{
+   public static void main (String[] args) throws FileNotFoundException{
          
       // finds board size, initializes board, and reads in ints  
       Scanner input = new Scanner(new File(FILE));      
@@ -24,57 +15,89 @@ public class FloodFill{
          input.nextLine();
          size++;
       }
-      board = new Integer[size][size];
+      board = new Square[size][size];
       Scanner input2 = new Scanner(new File(FILE));
       for (int i = 0; i < size; i++){
          for (int j = 0; j < size; j++){
-            board[i][j] = input2.nextInt();
+            board[i][j] = new Square(input2.nextInt(), i, j);
          }
       }    
-           
-      System.out.println(getString());      
-      move(0, 0, true);
+      
+      int moveCount = 0;
+   
+      System.out.println("Welcome to the Flood Fill Game! The goal of the game is to make all the squares on");
+      System.out.println("the board the same number in the same number in the smallest possible number of moves.");
+      System.out.println("A move consistents of taking a block, consisting of square and all squares of the same");
+      System.out.println("value that touch it vertically and horizontally, and incrementing or decrementing them.\n");
+   
+      while (!won()){
+         moveCount++;
+         System.out.println(getString());  
+         Scanner scan = new Scanner (System.in);
+         System.out.println();         
+         int col = -1;
+         while (col < 0 || col > size){
+            System.out.print("Enter the vertical column of your square (0 to " + size + "): ");
+            col = scan.nextInt();
+         }
+         int row = -1;
+         while (row < 0 || row > size){
+            System.out.print("Enter the horizonal row of your square (0 to " + size + "): ");    
+            row = scan.nextInt();
+         }
+         boolean increment;
+         System.out.print("Enter 1 to increment or 2 to decrement: ");
+         increment = (scan.nextInt() == 1);
+         
+         move(board[row][col], increment);
+      }   
+      
       System.out.println(getString());
-      move(0, 0, true);
-      System.out.println(getString());     
+      System.out.println("\nCongratulations! You won the game in " + moveCount + " moves.");
+      
    }
    
-   // increments all parts of the block containing square i,j
-   private static void move(int i, int j, boolean add){
-      int start = board[i][j];
-      Queue<Duple> queue = new ArrayDeque<>();
-      queue.add(new Duple(i, j));
-      while (!queue.isEmpty()){
-         Duple square = queue.remove();
-         if (square.row+1 < size)
-            if (board[square.row+1][square.col] == board[square.row][square.col])
-               queue.add(new Duple(square.row+1, square.col));
-         if (square.row-1 > 0)
-            if (board[square.row-1][square.col] == board[square.row][square.col])
-               queue.add(new Duple(square.row-1, square.col));
-         if (square.col+1 < size)
-            if (board[square.row][square.col+1] == board[square.row][square.col])
-               queue.add(new Duple(square.row, square.col+1));
-         if (square.col-1 > 0)
-            if (board[square.row][square.col-1] == board[square.row][square.col])
-               queue.add(new Duple(square.row, square.col-1));
-         if (start == board[square.row][square.col] && add)
-            board[square.row][square.col]++;
-         else 
-            board[square.row][square.col]--;
+   // returns an ArrayList of all the Squares in the same block as the parameter
+   private static ArrayList<Square> getBlock (Square s){
+      ArrayList<Square> list = new ArrayList<>();
+      int location = 0;
+      list.add(s);
+      // for each element in list, check all 4 surrounding squares
+      // if within bounds, not yet added, and value == param, add to list
+      while (location < list.size()){
+         Square cur = list.get(location);
+         location++;
+         if (cur.getRow()+1 < size && !list.contains(board[cur.getRow()+1][cur.getCol()]) && s.getValue() == board[cur.getRow()+1][cur.getCol()].getValue())
+            list.add(board[cur.getRow()+1][cur.getCol()]);
+         if (cur.getRow()-1 >= 0 && !list.contains(board[cur.getRow()-1][cur.getCol()]) && s.getValue() == board[cur.getRow()-1][cur.getCol()].getValue())
+            list.add(board[cur.getRow()-1][cur.getCol()]);
+         if (cur.getCol()+1 < size && !list.contains(board[cur.getRow()][cur.getCol()+1]) && s.getValue() == board[cur.getRow()][cur.getCol()+1].getValue())
+            list.add(board[cur.getRow()][cur.getCol()+1]);
+         if (cur.getCol()-1 >= 0 && !list.contains(board[cur.getRow()][cur.getCol()-1]) && s.getValue() == board[cur.getRow()][cur.getCol()-1].getValue())
+            list.add(board[cur.getRow()][cur.getCol()-1]);
       }
-      if (add)
-         start++;
-      else 
-         start--;
+      return list;
+   }
+   
+   // increments or decrements all squares in the block containing param s
+   private static void move (Square s, boolean increase){
+      ArrayList<Square> list = getBlock(s);
+      if (increase){
+         for (Square cur : list)
+            cur.setValue(cur.getValue()+1);
+      } 
+      else {
+         for (Square cur : list)
+            cur.setValue(cur.getValue()-1);         
+      }
    }
    
    // returns true iff the board contains only 1 number
-   private boolean won(){
-      int first = board[0][0];
-      for(Integer[] arr : board)
-         for(Integer i : arr)
-            if (i != first)
+   private static boolean won(){
+      int first = board[0][0].getValue();
+      for(Square[] arr : board)
+         for(Square i : arr)
+            if (i.getValue() != first)
                return false;
       return true;
    }
@@ -82,11 +105,15 @@ public class FloodFill{
    // returns String representation of the board
    private static String getString(){
       String s = "";
-      for(Integer[] arr : board){
-         for(Integer i : arr){
-            s += i + " ";
-         }
-         s += "\n";           
+      s += "   ";
+      for (int i = 0; i < size; i++){
+         s += i + " ";
+      }
+      for(int i = 0; i < size; i++){
+         s += "\n" + i + ": " ; 
+         for(Square cur : board[i]){
+            s += cur.getValue() + " ";
+         }       
       }
       return s;
    }
---
+++ Square.java
@@ -0,0 +1,27 @@
+public class Square {
+   int value;
+   int row;
+   int col;   
+   
+   public Square(int v, int a, int b){
+      value = v;
+      row = a;
+      col = b;
+   }
+   
+   public void setValue(int newValue){
+      value = newValue;
+   }
+   
+   public int getValue(){
+      return value;
+   }
+   
+   public int getRow(){
+      return row;
+   }
+   
+   public int getCol(){
+      return col;
+   }
+}
